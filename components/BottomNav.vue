a2864584bcffa6853a5356bf14b88392c241d07e
Rafael Amed Valenzuela <69322700+rafamedtech@users.noreply.github.com>
2022-08-18 16:25:53 -0700

    nav updated

--- components/BottomNav.vue
+++ components/BottomNav.vue
@@ -2,15 +2,17 @@
 
 <template>
   <div class="flex w-screen justify-center">
-    <ul class="menu rounded-box menu-horizontal fixed bottom-2 mx-auto bg-accent/75 text-base-100">
+    <ul
+      class="menu rounded-box menu-horizontal fixed bottom-2 mx-auto gap-0 bg-accent/75 text-base-100"
+    >
       <li>
-        <a class="flex flex-col"><i class="fa-solid fa-house text-xl"></i> Home</a>
+        <a class="flex flex-col gap-0"><i class="fa-solid fa-house text-xl"></i> Home</a>
       </li>
       <li>
-        <a class="flex flex-col"><i class="fa-solid fa-receipt text-xl"></i> Promotions</a>
+        <a class="flex flex-col gap-0"><i class="fa-solid fa-receipt text-xl"></i> Promotions</a>
       </li>
       <li>
-        <a class="flex flex-col"><i class="fa-solid fa-circle-info text-xl"></i> About</a>
+        <a class="flex flex-col gap-0"><i class="fa-solid fa-circle-info text-xl"></i> About</a>
       </li>
     </ul>
   </div>
