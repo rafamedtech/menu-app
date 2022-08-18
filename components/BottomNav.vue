fbcb64b73daac584411ccc99200f6f34c7b92a54
Rafael Amed Valenzuela <69322700+rafamedtech@users.noreply.github.com>
2022-08-18 16:41:42 -0700

    bottom navbar updated

--- components/BottomNav.vue
+++ components/BottomNav.vue
@@ -6,13 +6,15 @@
       class="menu rounded-box menu-horizontal fixed bottom-2 mx-auto gap-4 bg-accent/75 px-4 py-2 text-base-100"
     >
       <li>
-        <a class="flex flex-col p-0"><i class="fa-solid fa-house text-xl"></i> Home</a>
+        <a class="flex flex-col gap-0 p-0"><i class="fa-solid fa-house text-xl"></i> Home</a>
       </li>
       <li>
-        <a class="flex flex-col p-0"><i class="fa-solid fa-receipt text-xl"></i> Promotions</a>
+        <a class="flex flex-col gap-0 p-0"
+          ><i class="fa-solid fa-receipt text-xl"></i> Promotions</a
+        >
       </li>
       <li>
-        <a class="flex flex-col p-0"><i class="fa-solid fa-circle-info text-xl"></i> About</a>
+        <a class="flex flex-col gap-0 p-0"><i class="fa-solid fa-circle-info text-xl"></i> About</a>
       </li>
     </ul>
   </div>
