db9b9e25e5ef42e4f285fa1ab69d083711b67bdf
Rafael Amed Valenzuela <69322700+rafamedtech@users.noreply.github.com>
2022-08-25 10:59:26 -0700

    ssr true

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@ import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  ssr: false,
   css: ['@/assets/css/main.css'],
 
   // plugins: ['~/plugins/pinia-persist.js'],
